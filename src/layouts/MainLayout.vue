4c93a74ce79d62e479ff20a2a3cfc6da3b8fd36c
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-06-06 22:35:35 +0300

    feat: adds Simple Layout system

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div>
+  <!--Header-->
+  <header>
+    Header
+  </header>
+  <!--Content-->
+  <main>
+    Content
+  </main>
+  <!--Footer-->
+  <footer>
+    Footer
+  </footer>
+</div>
+</template>
---
+++ src/pages/demo/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import MainLayout from '@/layouts/MainLayout.vue'
+</script>
+
+<template>
+<main-layout>
+  Home page
+</main-layout>
+</template>
+
+<style scoped>
+
+</style>
