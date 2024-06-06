71f7f1b05af2cfe660591888f36dc24671368564
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-06-06 21:49:24 +0300

    feat: adds Primevue

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from "primevue/config";
+import Wind from "./presets/wind/index";
+import StyleClass from "primevue/styleclass";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.directive("styleclass", StyleClass);
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Wind
+});
 
 app.mount('#app')
